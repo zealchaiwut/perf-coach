728a9b39caf6533660ae48debce480b94c410ed9
Chaiwut Chaianuchittrakul <svictory88@gmail.com>
2026-05-28 19:03:35 +0700

    guard a9b0

--- alembic/versions/a9b0c1d2e3f4_create_daily_readiness_table.py
+++ alembic/versions/a9b0c1d2e3f4_create_daily_readiness_table.py
@@ -18,10 +18,10 @@ Includes:
 """
 from typing import Sequence, Union
 
-import sqlalchemy as sa
 from alembic import op
-from sqlalchemy.dialects import postgresql
+import sqlalchemy as sa
 from sqlalchemy import inspect
+from sqlalchemy.dialects import postgresql
 
 revision: str = "a9b0c1d2e3f4"
 down_revision: Union[str, None] = "f6a7b8c9d0e1"
