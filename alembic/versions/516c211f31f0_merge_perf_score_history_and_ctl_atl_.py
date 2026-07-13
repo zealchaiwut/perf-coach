85aa55f2459a01ce22d9b1a35c5deb44b5e8808c
zealchaiwut <svictory88@gmail.com>
2026-07-13 16:32:22 +0700

    style: remove unused imports from merge migration (issue #1361)

--- alembic/versions/516c211f31f0_merge_perf_score_history_and_ctl_atl_.py
+++ alembic/versions/516c211f31f0_merge_perf_score_history_and_ctl_atl_.py
@@ -7,9 +7,6 @@ Create Date: 2026-07-13 16:30:19.960655
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = '516c211f31f0'
